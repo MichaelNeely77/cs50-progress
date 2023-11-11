e844bc1c7623a773f1f6cb4acf6d90847c0a727c
MichaelNeely77 <michael@michaelneely.net>
2023-11-11 15:58:52 -0500

    Added the course lecture up to 3:42:00

---
+++ meow.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) 
+{
+    // printf("meow\n");
+    // printf("meow\n");
+    // printf("meow\n");
+
+    // int counter = 3;
+    // while (counter > 0) {
+    //     printf("meow\n");
+    //     counter = counter -1;
+    // }
+
+    //     int i = 3;
+    // while (i > 0) {
+    //     printf("meow\n");
+    //     i--;
+    // }
+
+    // int i = 1;
+    // while (i <= 3) {
+    //     printf("meow\n");
+    //     i++;
+    // }
+
+    //  int i = 0;
+    // while (i < 3) {
+    //     printf("meow\n");
+    //     i++;
+    // }
+
+    for (int i = 0; i < 3; i++) {
+        printf("mewow\n");
+    }
+
+
+}
+
+// in C indents are four sapces by convention
