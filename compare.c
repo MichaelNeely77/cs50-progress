a35805ba467199b218a5a5d7b797a788748e17eb
MichaelNeely77 <michael@michaelneely.net>
2023-11-11 15:28:02 -0500

    Added more on the second lecture at 3:12;00

--- compare.c
+++ compare.c
@@ -3,5 +3,16 @@
 
 int main(void)
 {
-    get_int("What's x? ");
+    int x = get_int("What's x? ");
+    int y = get_int("What's y?");
+
+    if (x < y) {
+    printf("x is less than y\n");
+    } else if (x > y) {
+    printf("x is not less than y\n");
+    } else {
+    printf("x is equal to y\n");
+
+    }
 }
+
