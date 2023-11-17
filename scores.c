071548482eddfd9775c27d03c712e700e278437f
MichaelNeely77 <michael@michaelneely.net>
2023-11-17 10:29:46 -0500

    Added arrays in C

--- scores.c
+++ scores.c
@@ -1,11 +1,26 @@
+#include <cs50.h>
 #include <stdio.h>
+
+const int N = 3;
+
+float average(int length, int array[]);
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[N];
+     for (int i = 0; i < N; i++) {
+        scores[i] = get_int("Score: ");
+     }
 
-    printf("Average: %i\n", (score1 + score2 + score3) / 3);
+    printf("Average: %f\n", average(N, scores));
+}
+
+float average(int length, int array[])
+{
+    int sum = 0;
+    for ( int i = 0; i < N; i++) {
+        sum = sum +array[i];
+    }
+    return sum / (float) N;
 }
 
