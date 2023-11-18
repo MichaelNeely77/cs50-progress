c3a6c1f33941d180cbe5be183314f0880ceff958
MichaelNeely77 <michael@michaelneely.net>
2023-11-18 14:36:33 -0500

    Added teh hi.c file which went over that a string is an array of chars

---
+++ hi.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) 
+{
+    // char c1 = 'H';
+    // char c2 = 'I';
+    // char c3 = '!';
+
+    string words[2];
+
+    words[0] = "HI!";
+    words[1] = "BYE!";
+
+
+    printf("%s \n", words[0]);
+    printf("%s \n", words[1]);
+
+    
+}
