249e9a8572a6190968e7188902c200e87509f588
MichaelNeely77 <michael@michaelneely.net>
2023-11-22 11:54:14 -0500

    Refined the for loop and added another library to make the code more efficient

--- uppercase.c
+++ uppercase.c
@@ -1,4 +1,5 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -6,16 +7,10 @@ int main(void)
 {
     string s = get_string("Before: ");
     printf("After: ");
-    for (int i = 0; i < strlen(s); i++)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if (s[i] >= 'a' && s[i] <= 'z') 
-        {
-            printf("%c", s[i] - 32);
-        } 
-        else 
-        {
-            printf("%c", s[i]);
-        }
+            printf("%c", toupper(s[i]));
     }
+    printf("\n");
 
 }
